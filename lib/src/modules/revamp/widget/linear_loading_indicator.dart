2744c412490cda4f7675458fa8efdf0b913823fc
Ifeakachukwu Osakwe <ifeakachukwuosakwe@gmail.com>
2025-07-23 11:50:57 +0100

    version 3.1.1

--- lib/src/modules/revamp/widget/linear_loading_indicator.dart
+++ lib/src/modules/revamp/widget/linear_loading_indicator.dart
@@ -4,18 +4,46 @@ import 'package:rex_app/src/modules/revamp/utils/config/theme/app_colors.dart';
 class LinearLoadingIndicator extends StatelessWidget {
   final bool loading;
 
-  const LinearLoadingIndicator({
-    required this.loading,
-    super.key,
-  });
+  const LinearLoadingIndicator({required this.loading, super.key});
 
   @override
   Widget build(BuildContext context) {
     return loading
         ? const LinearProgressIndicator(
-            minHeight: 2,
-            backgroundColor: Colors.white,
-            valueColor: AlwaysStoppedAnimation<Color>(AppColors.rexBlue))
+          minHeight: 2,
+          backgroundColor: Colors.white,
+          valueColor: AlwaysStoppedAnimation<Color>(AppColors.rexBlue),
+        )
         : const SizedBox.shrink();
+  }
+}
+
+class CircularLoader extends StatelessWidget {
+  final double? height;
+  final double? width;
+  final double? strokeWidth;
+  final Color? color;
+  const CircularLoader({
+    super.key,
+    this.height,
+    this.width,
+    this.strokeWidth,
+    this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height ?? 5,
+      width: width ?? 5,
+      child: Center(
+        child: CircularProgressIndicator(
+          strokeWidth: strokeWidth ?? 5,
+          color: color ?? AppColors.rexPurpleLight,
+          backgroundColor: Colors.grey.shade300,
+          strokeCap: StrokeCap.round,
+        ),
+      ),
+    );
   }
 }
