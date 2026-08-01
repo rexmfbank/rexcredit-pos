50dbedf3eb8ba549b5b47b8dad5944183e48a6e6
Ify Osakwe <ifeakachukwuosakwe@gmail.com>
2026-08-01 11:03:50 +0100

    fixes on transaction screen

--- lib/src/modules/login/login_transaction/login_trans_history.dart
+++ lib/src/modules/login/login_transaction/login_trans_history.dart
@@ -1,109 +1,109 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:rex_app/src/modules/api/rex_api.dart';
-import 'package:rex_app/src/modules/login/login_transaction/login_trans_history_item.dart';
-import 'package:rex_app/src/modules/login/provider/user_recent_transaction_provider.dart';
-import 'package:rex_app/src/modules/utils/general/app_strings.dart';
-import 'package:rex_app/src/modules/utils/general/constants.dart';
-import 'package:rex_app/src/modules/utils/widgets/app_scaffold.dart';
-import 'package:rex_app/src/modules/utils/widgets/appbar_sub_screen.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:hooks_riverpod/hooks_riverpod.dart';
+// import 'package:rex_app/src/modules/api/rex_api.dart';
+// import 'package:rex_app/src/modules/login/login_transaction/login_trans_history_item.dart';
+// import 'package:rex_app/src/modules/login/provider/user_recent_transaction_provider.dart';
+// import 'package:rex_app/src/modules/utils/general/app_strings.dart';
+// import 'package:rex_app/src/modules/utils/general/constants.dart';
+// import 'package:rex_app/src/modules/utils/widgets/app_scaffold.dart';
+// import 'package:rex_app/src/modules/utils/widgets/appbar_sub_screen.dart';
 
-class LoginTransHistory extends ConsumerStatefulWidget {
-  const LoginTransHistory({super.key});
+// class LoginTransHistory extends ConsumerStatefulWidget {
+//   const LoginTransHistory({super.key});
 
-  @override
-  ConsumerState<ConsumerStatefulWidget> createState() =>
-      _LoginTransHistoryState();
-}
+//   @override
+//   ConsumerState<ConsumerStatefulWidget> createState() =>
+//       _LoginTransHistoryState();
+// }
 
-class _LoginTransHistoryState extends ConsumerState<LoginTransHistory> {
-  @override
-  Widget build(BuildContext context) {
-    final recentTransact = ref.watch(userRecentTransactionProvider);
-    return AppScaffold(
-      padding: EdgeInsets.all(0),
-      appBar: AppbarSubScreen(title: 'Transaction History', centerTitle: true),
-      body: Column(
-        children: [
-          // TransactionSearchFilter(
-          //   onChangedText: (query) async {
-          //     await ref.read(paginationProvider.notifier).applySearch(query);
-          //   },
-          //   onTap: () {
-          //     showFilterTransaction(
-          //       context: context,
-          //       onClickApply: () {
-          //         _applyFilters();
-          //         context.pop();
-          //       },
-          //       onResetDateFilter: () {
-          //         ref.read(paginationProvider.notifier).refresh();
-          //         context.pop();
-          //       },
-          //     );
-          //   },
-          // ),
-          SizedBox(height: 10),
-          Flexible(
-            fit: FlexFit.loose,
-            child: Container(
-              margin: EdgeInsets.all(16.ar),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(16.ar),
-              ),
-              child: recentTransact.when(
-                data: (TData tData) {
-                  if (tData.transactions == null) {
-                    return const TransText(text: Strings.getTransErr);
-                  }
-                  if (tData.transactions!.data.isEmpty) {
-                    return const TransText(text: Strings.noTransactions);
-                  }
-                  return ListView.builder(
-                    physics: const BouncingScrollPhysics(),
-                    itemCount: tData.transactions!.data.length,
-                    itemBuilder: (context, index) {
-                      return LoginTransactionHistoryItem(
-                        trans: tData.transactions!.data[index],
-                        canTap: true,
-                      );
-                    },
-                  );
-                },
-                error:
-                    (e, _) => Center(
-                      child: Text(
-                        Strings.getTransErr,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ),
-                loading:
-                    () => const Center(
-                      child: CupertinoActivityIndicator(radius: 20.0),
-                    ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+// class _LoginTransHistoryState extends ConsumerState<LoginTransHistory> {
+//   @override
+//   Widget build(BuildContext context) {
+//     final recentTransact = ref.watch(userRecentTransactionProvider);
+//     return AppScaffold(
+//       padding: EdgeInsets.all(0),
+//       appBar: AppbarSubScreen(title: 'Transaction History', centerTitle: true),
+//       body: Column(
+//         children: [
+//           // TransactionSearchFilter(
+//           //   onChangedText: (query) async {
+//           //     await ref.read(paginationProvider.notifier).applySearch(query);
+//           //   },
+//           //   onTap: () {
+//           //     showFilterTransaction(
+//           //       context: context,
+//           //       onClickApply: () {
+//           //         _applyFilters();
+//           //         context.pop();
+//           //       },
+//           //       onResetDateFilter: () {
+//           //         ref.read(paginationProvider.notifier).refresh();
+//           //         context.pop();
+//           //       },
+//           //     );
+//           //   },
+//           // ),
+//           SizedBox(height: 10),
+//           Flexible(
+//             fit: FlexFit.loose,
+//             child: Container(
+//               margin: EdgeInsets.all(16.ar),
+//               decoration: BoxDecoration(
+//                 color: Colors.white,
+//                 borderRadius: BorderRadius.circular(16.ar),
+//               ),
+//               child: recentTransact.when(
+//                 data: (TData tData) {
+//                   if (tData.transactions == null) {
+//                     return const TransText(text: Strings.getTransErr);
+//                   }
+//                   if (tData.transactions!.data.isEmpty) {
+//                     return const TransText(text: Strings.noTransactions);
+//                   }
+//                   return ListView.builder(
+//                     physics: const BouncingScrollPhysics(),
+//                     itemCount: tData.transactions!.data.length,
+//                     itemBuilder: (context, index) {
+//                       return LoginTransactionHistoryItem(
+//                         trans: tData.transactions!.data[index],
+//                         canTap: true,
+//                       );
+//                     },
+//                   );
+//                 },
+//                 error:
+//                     (e, _) => Center(
+//                       child: Text(
+//                         Strings.getTransErr,
+//                         style: const TextStyle(
+//                           fontSize: 16,
+//                           fontWeight: FontWeight.w500,
+//                         ),
+//                       ),
+//                     ),
+//                 loading:
+//                     () => const Center(
+//                       child: CupertinoActivityIndicator(radius: 20.0),
+//                     ),
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
 
-class TransText extends StatelessWidget {
-  const TransText({super.key, required this.text});
-  final String text;
-  @override
-  Widget build(BuildContext context) => Align(
-    alignment: Alignment.center,
-    child: Text(
-      text,
-      style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-    ),
-  );
-}
+// class TransText extends StatelessWidget {
+//   const TransText({super.key, required this.text});
+//   final String text;
+//   @override
+//   Widget build(BuildContext context) => Align(
+//     alignment: Alignment.center,
+//     child: Text(
+//       text,
+//       style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+//     ),
+//   );
+// }
--- lib/src/modules/login/login_transaction/login_trans_history_detail.dart
+++ lib/src/modules/login/login_transaction/login_trans_history_detail.dart
@@ -1,76 +1,76 @@
-import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:rex_app/src/modules/api/models/extension_on_payload.dart';
-import 'package:rex_app/src/modules/login/login_transaction/login_transaction_detail_widgets.dart';
-import 'package:rex_app/src/modules/pos_device/notifier/pos_global_notifier.dart';
-import 'package:rex_app/src/modules/quick_transaction/provider/pos_transactions_provider.dart';
-import 'package:rex_app/src/modules/utils/general/constants.dart';
-import 'package:rex_app/src/modules/utils/theme/app_colors.dart';
-import 'package:rex_app/src/modules/utils/widgets/app_scaffold.dart';
-import 'package:rex_app/src/modules/utils/widgets/appbar_sub_screen.dart';
-import 'package:rex_app/src/modules/utils/widgets/container_style_button.dart';
+// import 'package:flutter/material.dart';
+// import 'package:hooks_riverpod/hooks_riverpod.dart';
+// import 'package:rex_app/src/modules/api/models/extension_on_payload.dart';
+// import 'package:rex_app/src/modules/login/login_transaction/login_transaction_detail_widgets.dart';
+// import 'package:rex_app/src/modules/pos_device/notifier/pos_global_notifier.dart';
+// import 'package:rex_app/src/modules/quick_transaction/provider/pos_transactions_provider.dart';
+// import 'package:rex_app/src/modules/utils/general/constants.dart';
+// import 'package:rex_app/src/modules/utils/theme/app_colors.dart';
+// import 'package:rex_app/src/modules/utils/widgets/app_scaffold.dart';
+// import 'package:rex_app/src/modules/utils/widgets/appbar_sub_screen.dart';
+// import 'package:rex_app/src/modules/utils/widgets/container_style_button.dart';
 
-class LoginTransHistoryDetail extends ConsumerStatefulWidget {
-  const LoginTransHistoryDetail({super.key});
+// class LoginTransHistoryDetail extends ConsumerStatefulWidget {
+//   const LoginTransHistoryDetail({super.key});
 
-  @override
-  ConsumerState<ConsumerStatefulWidget> createState() =>
-      _LoginTransHistoryDetailState();
-}
+//   @override
+//   ConsumerState<ConsumerStatefulWidget> createState() =>
+//       _LoginTransHistoryDetailState();
+// }
 
-class _LoginTransHistoryDetailState
-    extends ConsumerState<LoginTransHistoryDetail> {
-  @override
-  Widget build(BuildContext context) {
-    final detail = ref.watch(memoryLoginTransProvider);
-    final state = ref.watch(posGlobalProvider);
-    //
-    return AppScaffold(
-      appBar: AppbarSubScreen(title: 'Transaction Details', centerTitle: true),
-      backgroundColor: AppColors.rexBackground,
-      body: ListView(
-        physics: const BouncingScrollPhysics(),
-        children: [
-          SizedBox(height: 16.ah),
-          LoginTransactionsDetailSummary(transaction: detail),
-          SizedBox(height: 16.ah),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Expanded(
-                child: ContainerStyleButton(
-                  title: 'Fetch Status',
-                  bgColor: AppColors.rexWhite,
-                  textColor: Color(0xff002766),
-                  border: Border.all(color: Color(0xff002766)),
-                  onTap: () {},
-                ),
-              ),
-              SizedBox(width: 8),
-              Expanded(
-                child: ContainerStyleButton(
-                  title: 'Print Receipt',
-                  bgColor: state.canPrint ? Color(0xff002766) : AppColors.grey,
-                  textColor: AppColors.rexWhite,
-                  onTap:
-                      state.canPrint
-                          ? () {
-                            ref
-                                .read(posGlobalProvider.notifier)
-                                .printTransDetail(detail.toPrintObj());
-                          }
-                          : null,
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: 8.ah),
-          detail.statusId == null
-              ? SizedBox.shrink()
-              : LoginTransactionReportButton(transaction: detail),
-          SizedBox(height: 4.ah),
-        ],
-      ),
-    );
-  }
-}
+// class _LoginTransHistoryDetailState
+//     extends ConsumerState<LoginTransHistoryDetail> {
+//   @override
+//   Widget build(BuildContext context) {
+//     final detail = ref.watch(memoryLoginTransProvider);
+//     final state = ref.watch(posGlobalProvider);
+//     //
+//     return AppScaffold(
+//       appBar: AppbarSubScreen(title: 'Transaction Details', centerTitle: true),
+//       backgroundColor: AppColors.rexBackground,
+//       body: ListView(
+//         physics: const BouncingScrollPhysics(),
+//         children: [
+//           SizedBox(height: 16.ah),
+//           LoginTransactionsDetailSummary(transaction: detail),
+//           SizedBox(height: 16.ah),
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//             children: [
+//               Expanded(
+//                 child: ContainerStyleButton(
+//                   title: 'Fetch Status',
+//                   bgColor: AppColors.rexWhite,
+//                   textColor: Color(0xff002766),
+//                   border: Border.all(color: Color(0xff002766)),
+//                   onTap: () {},
+//                 ),
+//               ),
+//               SizedBox(width: 8),
+//               Expanded(
+//                 child: ContainerStyleButton(
+//                   title: 'Print Receipt',
+//                   bgColor: state.canPrint ? Color(0xff002766) : AppColors.grey,
+//                   textColor: AppColors.rexWhite,
+//                   onTap:
+//                       state.canPrint
+//                           ? () {
+//                             ref
+//                                 .read(posGlobalProvider.notifier)
+//                                 .printTransDetail(detail.toPrintObj());
+//                           }
+//                           : null,
+//                 ),
+//               ),
+//             ],
+//           ),
+//           SizedBox(height: 8.ah),
+//           detail.statusId == null
+//               ? SizedBox.shrink()
+//               : LoginTransactionReportButton(transaction: detail),
+//           SizedBox(height: 4.ah),
+//         ],
+//       ),
+//     );
+//   }
+// }
